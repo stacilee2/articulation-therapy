ce26accb2d6096159ee1400ec413408b55d617bb
Staci Rosales <stacileep2@gmail.com>
2023-02-18 11:03:06 -0600

    update Games component with links to games

---
+++ articulation-therapy/src/Games.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Games() {
+  return (
+    <div className="games">
+      
+      <ul className="games-list">
+      <p>Find links to different Vocalic R Games Here:</p>
+      <a className="App-link" href="https://wordwall.net/resource/29254522/speech-therapy/vocalic-ar">Vocalic -AR Game</a><br></br>
+      <a className="App-link" href="https://wordwall.net/resource/14235591/air-picture-match">Vocalic -AIR Game</a><br></br>
+      <a className="App-link" href="https://wordwall.net/resource/19024918/vocalic-r-or">Vocalic -OR Game</a><br></br>
+      <a className="App-link" href="https://wordwall.net/resource/30802764/vocalic-r-matching-ire">Vocalic -IRE Game</a><br></br>
+      <a className="App-link" href="https://wordwall.net/resource/14233002/open-the-box-final-er-">Vocalic -ER Game</a><br></br>
+      <a className="App-link" href="https://wordwall.net/resource/38341723/vocalic-r-ear-words">Vocalic -EAR Game</a><br></br>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Games;
