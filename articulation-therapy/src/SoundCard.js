73ecec474b19f7667da44ef40bcd909b26eaef6e
stacilee2 <111379269+stacilee2@users.noreply.github.com>
2023-02-26 13:36:27 -0600

    Update SoundCard.js

--- articulation-therapy/src/SoundCard.js
+++ articulation-therapy/src/SoundCard.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 function SoundCard( { sound } ) {
 
+    let words = sound.words;
+    console.log(words)
+    let wordsArray = words.split(",");
+
   return (
     <div className="card">
       <h2 key={sound.name}>{sound.name}</h2>
-      <p> Words: {sound.words}</p>
+      <p> Check each word as you practice! </p>
+      <p> Words: </p> 
+      { wordsArray.map(word => {
+        return <ul className="word" key={word}>{word}
+        <input type="checkbox" className="checkbox"></input></ul>
+      })}
         <p key={sound.book}> Book: {sound.book} </p>
-        <a href={sound.url}> Read-Aloud: {sound.url} </a>
+        <a href={sound.url} className="book-url"> Read-Aloud: {sound.url} </a>
       </div>
   )
 }
