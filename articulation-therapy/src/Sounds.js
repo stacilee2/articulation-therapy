3870af6ddbb5179a8c15f15621d9451d21dc60c0
stacilee2 <111379269+stacilee2@users.noreply.github.com>
2023-02-27 16:08:57 -0600

    Update Sounds.js

--- articulation-therapy/src/Sounds.js
+++ articulation-therapy/src/Sounds.js
@@ -6,9 +6,9 @@ function Sounds( {sounds} ) {
   return (
     <div className="collection">
       <h4>Find different vocalic /r/ sounds with instructions for target level here based on your client's needs:</h4>
-      <li>Word Level: target word only 5-10 reps</li>
-      <li>Phrase Level: I see...(add target here) / My...(add target here)</li>
-      <li>Sentence Level: Create a sentence using target</li>
+      <li>WORD Level: target word only 5-10 reps</li>
+      <li>PHRASE Level: I see..." "   OR   My..." "</li>
+      <li>SENTENCE Level: Create a sentence using target</li>
      {
       sounds.map((sound) => {
         return (
