345eb1945c2a9a8188f23c7252da3d8563bd2dbb
stacilee2 <111379269+stacilee2@users.noreply.github.com>
2023-03-21 17:21:53 -0500

    Update AddSound.js

--- articulation-therapy/src/AddSound.js
+++ articulation-therapy/src/AddSound.js
@@ -6,7 +6,7 @@ function AddSound( {onAddSound} ) {
     const [words, setWords] = useState("");
     const [book, setBook] = useState("");
     const [url, setUrl] = useState("");
-    const [errors, setErrors] = useState([])
+    const [errors, setErrors] = useState("")
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,7 +16,7 @@ function AddSound( {onAddSound} ) {
           handlePost()
 
         } else {
-          setErrors(["PLEASE INCLUDE COMMAS BETWEEN EACH TARGET WORD!"],
+          setErrors(["PLEASE INCLUDE 5 OR MORE WORDS AND COMMAS BETWEEN EACH TARGET WORD!"],
           setTimeout(() => {
             setErrors("")
           }, 5000));
@@ -29,7 +29,7 @@ function AddSound( {onAddSound} ) {
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ name, words, book, url}),
+            body: JSON.stringify({ name, words, book, url }), 
             
           })
           .then ((r) => r.json())
@@ -41,61 +41,58 @@ function AddSound( {onAddSound} ) {
           setUrl("")
         }
     }
-        
-  
+
   return (
-    <div className="container">
-      { errors.length > 0 ? errors.map((error)=> (
-        <p key={error} className="error" style={{ color: "red" }}> {error}</p>
+    <div>
+      { errors.length > 0 ? errors.map((errorMessage)=> (
+        <p key={errorMessage} className="error" style={{ color: "red" }}> {errorMessage}</p>
       )) : null }
       <form className="form" onSubmit={handleSubmit}>
-        <h3>Want to individualize your session? Create your own sound card here!</h3>
+        <h3>Want to individualize your session?</h3>
+        <h4>Create your own sound card here!</h4>
+        <label className="label"> Sound Target (ex: p or k)</label><br></br>
         <input
           type="text"
           name="name"
           onChange={(e) => setName(e.target.value)}
           value={name}
-          placeholder="Enter a sound..."
-          className="input-text"
+          placeholder=""
         />
         <br />
+        <label className="label"> Five or More Target Words (include commas please)</label><br></br>
         <input
           type="text"
           name="name"
           onChange={(e) => setWords(e.target.value)}
           value={words}
-          placeholder="Enter target target words"
-          className="input-text"
+          placeholder=""
         />
         <br />
+        <label className="label"> Title of Book and/or Author's Name</label><br></br>
         <input
           type="text"
           name="image"
           onChange={(e) => setBook(e.target.value)}
           value={book}
-          placeholder="Enter a book"
-          className="input-text"
+          placeholder=""
         />
         <br /> 
+        <label className="label"> Read-aloud Video Link</label><br></br>
         <input
           type="text"
           name="image"
           onChange={(e) => setUrl(e.target.value)}
           value={url}
-          placeholder="Enter a book video URL"
-          className="input-text"
+          placeholder=""
         />
-        <br/><br></br>
-        <h4>After you submit, find your sound card on the Sounds page! Enjoy!</h4>
+        <br />
+        <h4>After you submit, find your sound card on the <a href="http://localhost:3001/sounds">Sounds</a> page! Enjoy!</h4>
         <input
           type="submit"
           name="submit"
           value="Create Sound Card"
-          className="submit"
         />
-       
       </form>
-      
     </div>
   );
 }
