9f31f39b93dfcc4584f1e9bdf7f349843c161c38
stacilee2 <111379269+stacilee2@users.noreply.github.com>
2023-03-26 22:32:59 -0500

    Update SoundCard.js with Link

--- articulation-therapy/src/SoundCard.js
+++ articulation-therapy/src/SoundCard.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
   function SoundCard( { sound } ) {
-
+    
     const { name, book, url, words } = sound
     let wordsArray = words.split(",");
 
-  return (
-    <div className="card">
-      <h2 key={name}>{name}</h2>
-      <p> Check each word as you practice! </p>
-      <p> Words: </p> 
-      { wordsArray.map((word) => {
-        return <ul className="word" key={word}> {word}
-        <input type="checkbox" className="checkbox" key={word}></input></ul>
-      })}
-        <p key={book}> Book:{book} </p>
-        <a href={url} className="book-url"> Read-Aloud: {url} </a>
-      </div>
-  )
+    console.log(url)
+
+    return (
+      <div className="card">
+        <h2 key={name}>{name}</h2>
+        <p> Check each word as you practice! </p>
+        <p> Words: </p> 
+        { wordsArray.map((word) => {
+          return <ul className="word" key={word}> {word}
+          <input type="checkbox" className="checkbox" key={word}></input></ul>
+        })}
+          <p key={book}> Book: {book} </p>
+          <Link to={{ pathname: url }} target={url} >{url}</Link>
+        </div>
+    )
 }
 export default SoundCard;
 
